b829e82c7448462cd8ee6846f4aa06a7c243dcb8
Carmen Pelayo <90655687+carmenpelayo@users.noreply.github.com>
2023-11-20 12:25:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, jsonify
-from paper_scanner.arxiv_scraper import fetch_papers
-
-app = Flask(__name__)
-
-@app.route('/api/papers')
-def get_papers():
+@app.route('/scrape')
+def scrape_papers():
     try:
-        papers = fetch_papers()
-        return jsonify(papers)
+        scraped_papers = scrape_arxiv_papers()
+        return jsonify(scraped_papers)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/filter')
+def filter_papers():
+    try:
+        # Assuming you need to pass the scraped papers to the filter function
+        # You might need to adjust this based on how your scripts work
+        papers_to_filter = scrape_arxiv_papers()
+        filtered_papers = filter_arxiv_papers(papers_to_filter)
+        return jsonify(filtered_papers)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
